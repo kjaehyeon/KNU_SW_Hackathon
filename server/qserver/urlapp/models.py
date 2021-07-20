6695982f257667054e4a48b452e7d6762b97ee92
Ktaewon <dydalehdxo@gmail.com>
2021-07-20 21:28:16 +0900

    cliplist api finish

--- server/qserver/urlapp/models.py
+++ server/qserver/urlapp/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Folder(models.Model):
-    User = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=200)
     def __str__(self):
         return self.name
