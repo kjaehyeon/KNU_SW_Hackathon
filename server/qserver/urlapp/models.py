1227cb39aab393c8722c0904ee1da593a704650b
Ktaewon <dydalehdxo@gmail.com>
2021-07-22 00:49:58 +0900

    urlapp finish

--- server/qserver/urlapp/models.py
+++ server/qserver/urlapp/models.py
@@ -1,20 +1,55 @@
 from django.db import models
 from django.contrib.auth.models import User
+import metadata_parser
+from urllib.parse import urlparse
+
+from django.core.files import File
+
+from .file import download, get_buffer_ext # 위에서 만든 file.py 경로
 
 # Create your models here.
 class Folder(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, related_name='folders', on_delete=models.CASCADE, blank=False)
     name = models.CharField(max_length=200)
     def __str__(self):
-        return self.name
+        return f'[{self.pk}]{self.name}'
+
+def get_file_path(instance, filename):
+    return '/'.join([instance.user.username, "URLAPP", instance.folder.name, instance.title, filename])
 
 class Url(models.Model):
-    folder = models.ForeignKey(Folder, default =None ,on_delete=models.CASCADE)
-    title = models.CharField(max_length=200)
-    body = models.TextField()
-    img = models.ImageField()
     link = models.URLField()
+    page = metadata_parser.MetadataParser(url=link.name)
+    title = models.CharField(max_length=200, null=True)
+    folder = models.ForeignKey(Folder, on_delete=models.CASCADE, blank=False)
+    user = models.ForeignKey(User, related_name='urls', blank=False, on_delete=models.CASCADE)
+    body = models.TextField(null=True)
+    image_url = models.URLField(null=True)
+    #img = models.ImageField(upload_to=get_file_path, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    """
+    def save(self, *args, **kwargs):
+        # ImageField에 파일이 없고, url이 존재하는 경우에만 실행
+        if self.link and not self.img:
+            # 우선 purchase_url의 대표 이미지를 크롤링하는 로직은 생략하고, 크롤링 결과 이미지 url을 임의대로 설정  
+            page = metadata_parser.MetadataParser(self.link)
+            item_image_url = page.get_metadatas("image", strategy=['og'])[0]
+
+            if item_image_url:
+                temp_file = download(item_image_url)
+                file_name = '{urlparse}.{ext}'.format(
+                    # url의 마지막 '/' 내용 중 확장자 제거
+                    # ex) url = 'https://~~~~~~/bag-1623898_960_720.jpg'
+                    #     -> 'bag-1623898_960_720.jpg'
+                    #     -> 'bag-1623898_960_720'
+                    urlparse=urlparse(item_image_url).path.split('/')[-1].split('.')[0],
+                    ext=get_buffer_ext(temp_file)
+                )
+                self.img().save(file_name, File(temp_file))
+                super().save()
+            else:
+                super().save()
+    """
     def __str__(self):
-        return self.title
+        return f'[{self.pk}]{self.title}'
--- server/qserver/urlapp/views.py
+++ server/qserver/urlapp/views.py
@@ -2,9 +2,33 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework.decorators import action
+from rest_framework import permissions
 from .models import Url, Folder
 from .serializers import FolderSerializer, UrlSerializer
+from django.db import models
+from django.dispatch import receiver
+import os
+import metadata_parser
+from io import BytesIO
+import magic
+import requests
+from django.core.files import File
+from urllib.parse import urlparse
 # Create your views here.
+class IsOwnerOrReadOnly(permissions.BasePermission):
+    """
+    Object-level permission to only allow owners of an object to edit it.
+    Assumes the model instance has an `owner` attribute.
+    """
+
+    def has_object_permission(self, request, view, obj):
+        # Read permissions are allowed to any request,
+        # so we'll always allow GET, HEAD or OPTIONS requests.
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        # Instance must have an attribute named `owner`.
+        return obj.user == request.user
 
 class FolderViewSet(viewsets.ModelViewSet):
     #queryset = Folder.objects.all()
@@ -27,10 +51,57 @@ class FolderViewSet(viewsets.ModelViewSet):
         return Response(serializer.data)
 
 
-class UrlViewSet(viewsets.ModelViewSet):
-    queryset = Url.objects.all()
-    serializer_class = UrlSerializer
+# url로부터 파일을 임시 다운로드
+def download(url):
+    response = requests.get(url)
+    binary_data = response.content
+    temp_file = BytesIO()
+    temp_file.write(binary_data)
+    temp_file.seek(0)
+    return temp_file
 
+# 파일 확장자 추출
+def get_buffer_ext(buffer):
+    buffer.seek(0)
+    mime_info = magic.from_buffer(buffer.read(), mime=True)
+    buffer.seek(0)
+    return mime_info.split('/')[-1]
+
+class UrlViewSet(viewsets.ModelViewSet):
+    #queryset = Url.objects.all()
+    serializer_class = UrlSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+    def get_queryset(self):
+        user = self.request.user
+        folder = self.request.GET['name']
+        print(folder)
+        return Url.objects.filter(user__username = user, folder__name = folder)    
+    def perform_create(self, serializer):
+        print(self.request.data["link"])
+        page = metadata_parser.MetadataParser(self.request.data["link"])
+        """
+        item_image_url = page.get_metadatas("image", strategy=['og'])[0]
+        print(item_image_url)
+        temp_file = download(item_image_url)
+        file_name = '{urlparse}.{ext}'.format(
+            # url의 마지막 '/' 내용 중 확장자 제거
+            # ex) url = 'https://~~~~~~/bag-1623898_960_720.jpg'
+            #     -> 'bag-1623898_960_720.jpg'
+            #     -> 'bag-1623898_960_720'
+            urlparse=urlparse(item_image_url).path.split('/')[-1].split('.')[0],
+            ext=get_buffer_ext(temp_file)
+        )
+        #self.img.save(file_name, File(temp_file))  
+        #Url.img.save(file_name, File(temp_file))
+        """
+        serializer.save(
+            user=self.request.user, 
+            title=page.get_metadatas("title", strategy=['og'])[0],
+            body=page.get_metadatas("description", strategy=['og'])[0],
+            image_url=page.get_metadatas("image", strategy=['og'])[0]
+        )
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
 # @api_view(['GET']) # 해당 함수 view에서 처리할 http 메소드
 # def ClipAPI(request):
 #     #user = 
