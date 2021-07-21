8184b7e71de80043814c911c2551d27c66682842
hyunjun <nhj2927@naver.com>
2021-07-21 18:02:48 +0900

    404, home page 추가

---
+++ client/src/Pages/Home/index.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import Nav from 'Components/Nav';
+import Card from 'Components/Card';
+
+import {BsQuestionCircleFill} from 'react-icons/bs';
+import {FaRegUser} from 'react-icons/fa';
+import {FiLogOut} from 'react-icons/fi';
+
+import './style.scss';
+
+function Home() {
+  return (
+    <div className='home__container'>
+      <div className='aside'>
+        <div className='wrapper'>
+          <div className='logo'>
+            <BsQuestionCircleFill className='icon'/>
+            Query
+          </div>
+          <Nav/>
+        </div>
+      </div>
+      <div className='body'>
+        <div className='user'>
+          <FaRegUser className='icon'/>
+          사용자
+          <div className='logout'>
+            <FiLogOut className='icon'/>
+            로그아웃
+          </div>
+        </div>
+        <div className='main'>
+          <Card/>
+          <Card/>
+          <Card/>
+          <Card/>
+          <Card/>
+          <Card/>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
