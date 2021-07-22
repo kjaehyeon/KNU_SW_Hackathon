0f000f2725c9d3ec6636ab6e7a312812e057b7bc
hyunjun <nhj2927@naver.com>
2021-07-22 23:11:53 +0900

    api route 추가

--- client/src/Configs/api.js
+++ client/src/Configs/api.js
@@ -1,9 +1,10 @@
-const root_url = 'http://192.168.0.104:8000/';
-
-export const ADD_URL_FOLDER = `${root_url}/urlapp/cliplist`;
-export const ADD_LECTURE_FOLDER = `${root_url}/lecture/subject`;
-export const ADD_URL_FILE = `${root_url}/urlapp/clip`;
-export const ADD_LECTURE_FILE = `${root_url}/lecture/file`;
+export const ADD_URL_FOLDER = '/urlapp/cliplist/';
+export const DELETE_URL_FOLDER = '/urlapp/cliplist/delete/';
+export const ADD_LECTURE_FOLDER = '/lecture/subject/';
+export const ADD_URL_FILE = '/urlapp/clip/';
+export const ADD_LECTURE_FILE = '/lecture/file';
 export const REGISTER = '/certi/register';
 export const SIGNIN = '/certi/login';
 export const AUTH = '/certi/user';
+export const GET_URL_FOLDER_LIST = '/urlapp/cliplist/';
+export const GET_LECTURE_FOLDER_LIST = '/lecture/subject/';
