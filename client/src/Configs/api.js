9039550d598c6f37f519e69c7d25805985dcd241
hyunjun <nhj2927@naver.com>
2021-07-22 19:47:47 +0900

    api 경로 추가

---
+++ client/src/Configs/api.js
@@ -0,0 +1,9 @@
+const root_url = 'http://192.168.0.104:8000/';
+
+export const ADD_URL_FOLDER = `${root_url}/urlapp/cliplist`;
+export const ADD_LECTURE_FOLDER = `${root_url}/lecture/subject`;
+export const ADD_URL_FILE = `${root_url}/urlapp/clip`;
+export const ADD_LECTURE_FILE = `${root_url}/lecture/file`;
+export const REGISTER = '/certi/register';
+export const SIGNIN = '/certi/login';
+export const AUTH = '/certi/user';
