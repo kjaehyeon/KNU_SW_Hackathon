c393626f35805948f21b73e45ebf7d83c71a44e1
hyunjun <nhj2927@naver.com>
2021-07-23 08:17:00 +0900

    home page 코드 구조 수정

--- client/src/Pages/Home/index.js
+++ client/src/Pages/Home/index.js
@@ -1,94 +1,123 @@
 /*eslint-disable*/
 import React, { useEffect } from 'react';
 import { useState } from 'react';
+import { useHistory } from 'react-router';
 import axios from 'axios';
 
 import Nav from 'Components/Nav';
 import Card from 'Components/Card';
-import { GET_FOLDER_LIST, GET_LECTURE_FOLDER_LIST, GET_URL_FILE_LIST, GET_URL_FOLDER_LIST } from 'Configs/api';
+import Loader from "react-loader-spinner";
+import {GET_LECTURE_FILE_LIST, GET_LECTURE_FOLDER_LIST, GET_URL_FILE_LIST, GET_URL_FOLDER_LIST } from 'Configs/api';
 
-import {BsQuestionCircleFill} from 'react-icons/bs';
 import {FaRegUser} from 'react-icons/fa';
 import {FiLogOut} from 'react-icons/fi';
 
+import src from 'assets/logo.png';
+import query from 'assets/query.png';
 import './style.scss';
 
 function Home() {
-  const [update, setUpdate] = useState(true);
+  const history = useHistory();
+  const [loading, setLoading] = useState(true);
   const [url_folder_list, setUrlFolderList] = useState([]);
   const [lecture_folder_list, setLectureFolderList] = useState([]);
   const [file_list, setFileList] = useState([]);
 
   const path = decodeURI(location.pathname.slice(1));
   const type = path.split('-')[0];
-  const user_id = localStorage.getItem('username');
+  const user_id = localStorage.getItem('username'); 
 
-  useEffect (()=>{
-    if(path !== 'main'){
-      if(type === 'url'){
-        const name = path.split('-')[1];
-        axios({
-          headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Token ${document.cookie.split('=')[1]}`
-          },
-          method: 'get',
-          url: GET_URL_FILE_LIST,
-          params: {
-            name
-          }
-        }).then((res)=>{
-          setFileList(res.data);
-        }).catch((err)=>{
-          console.log(err);
-        });
-      } else{
-        const [grade, semester, subject] = path.split('-').slice(1);
-        console.log(grade, semester, subject);
+  useEffect (async ()=>{
+    setLoading(true);
+    if(type === 'url'){
+      const name = path.split('-')[1];
+      try{
+      const response = await axios({
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Token ${document.cookie.split('=')[1]}`
+        },
+        method: 'get',
+        url: GET_URL_FILE_LIST,
+        params: {
+          name
+        }
+      })
+      setFileList(response.data);
+      }catch(err){
+        console.log(err);
       }
-      
+    } else if(type === 'lecture'){
+      try {
+      const [grade, semester, subject] = path.split('-').slice(1);
+      const response = await axios({
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Token ${document.cookie.split('=')[1]}`
+        },
+        method: 'get',
+        url: GET_LECTURE_FILE_LIST,
+        params: {
+          grade: `${grade}학년`,
+          semester: `${semester}학기`,
+          subject,
+        }
+      });
+      setFileList(response.data);
+      } catch(err){
+        console.log(err);
+      }
     }
-    axios({
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': `Token ${document.cookie.split('=')[1]}`
-      },
-      method: 'get',
-      url: GET_URL_FOLDER_LIST,
-    }).then((res)=>{
-      setUrlFolderList(res.data);
-    }).catch((err)=>{
+    try{
+      const response = await axios({
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Token ${document.cookie.split('=')[1]}`
+        },
+        method: 'get',
+        url: GET_URL_FOLDER_LIST,
+      });
+      setUrlFolderList(response.data);
+    } catch(err){
       console.log(err);
-    });
+    }
 
-    axios({
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': `Token ${document.cookie.split('=')[1]}`
-      },
-      method: 'get',
-      url: GET_LECTURE_FOLDER_LIST,
-    }).then((res)=>{
-      setLectureFolderList(res.data);
-    }).catch((err)=>{
-      console.log(err);
-    });
-  }, []);
+    try{
+      const response = await axios({
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Token ${document.cookie.split('=')[1]}`
+        },
+        method: 'get',
+        url: GET_LECTURE_FOLDER_LIST,
+      });
+        setLectureFolderList(response.data);    
+    } catch(err){
+      console.log(err)
+    }
+    setLoading(false);
+  }, [path]);
+
+  const goHome = () => {
+    history.push('/main');
+  }
 
   return (
     <div className='home__container'>
       <div className='aside'>
         <div className='wrapper'>
-          <div className='logo'>
-            <BsQuestionCircleFill className='icon'/>
-            Query
+          <div
+            className='logo'
+            onClick={goHome}
+          >
+            <img width='100%' height='30%' src={src}/>
           </div>
           <Nav
             url_folder_list={url_folder_list}
             setUrlFolderList={setUrlFolderList}
             lecture_folder_list={lecture_folder_list}
             setLectureFolderList={setLectureFolderList}
-            setUpdate={setUpdate}
+            setLoading={setLoading}
           />
         </div>
       </div>
@@ -103,24 +132,55 @@ function Home() {
         </div>
         <div className='main'>
           {
+            loading ? <div className='loader'><Loader type='Bars' color='#607d8b'/></div> :  
+            file_list.length > 0 ?
             file_list.map((file, i)=>{
-              const {
-                title,
-                body,
-                image_url,
-                link
-              } = file;
-              return (
-              <Card
-                key={i}
-                type={type}
-                title={title}
-                summary={body}
-                img={image_url}
-                link={link}
-              />
-              );
-            })
+              if(type === 'url'){
+                const {
+                  title,
+                  body,
+                  image_url,
+                  link,
+                  id
+                } = file;
+                return (
+                <Card
+                  key={i}
+                  type={type}
+                  title={title}
+                  summary={body}
+                  img={image_url}
+                  link={link}
+                  id={id}
+                />
+                );
+              } else if(type === 'lecture') {
+                const {
+                  name,
+                  id,
+                  grade,
+                  semester,
+                  subject,
+                  file_data
+                } = file
+                
+                return (
+                  <Card
+                    key={i}
+                    title={name}
+                    grade={grade}
+                    type={type}
+                    semester={semester}
+                    subject={subject}
+                    file_data={file_data}
+                    id={id}
+                  />
+                )
+              }
+            }) : 
+            <div className='not'>
+              <img width='300px' height='300px' src={query}/>
+            </div>
           }
         </div>
       </div>
