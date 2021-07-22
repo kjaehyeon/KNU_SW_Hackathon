77084ea7811e62e2e721e7c7223efd66df2794ea
hyunjun <nhj2927@naver.com>
2021-07-23 08:15:58 +0900

    card 컴포넌트 기능 추가

--- client/src/Components/Card/index.js
+++ client/src/Components/Card/index.js
@@ -1,9 +1,14 @@
 /*eslint-disable*/
 import React from 'react';
+import axios from 'axios';
+import fileDownload from 'js-file-download';
 
 import {BsDownload, BsQuestionDiamond} from 'react-icons/bs';
 import {RiDeleteBin5Line} from 'react-icons/ri';
 import {GoLink} from 'react-icons/go';
+import {GrDocumentPdf, GrNotes} from 'react-icons/gr';
+
+import { DELETE_LECTURE_FILE, DELETE_URL_FILE } from 'Configs/api';
 
 import './style.scss';
 
@@ -13,11 +18,55 @@ function Card(props) {
     title,
     summary,
     img,
-    link
+    link,
+    id,
+    grade,
+    semester,
+    subject,
+    file_data
   } = props;
 
   const goLink = () => {
     open(link);
+  }
+
+  const deleteFile = async () => {
+    if(!confirm('삭제하시겠습니까?')){
+      return;
+    }
+    if(type === 'url'){
+    try{
+      await axios({
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Token ${document.cookie.split('=')[1]}`
+        },
+        method: 'delete',
+        url: `${DELETE_URL_FILE}${id}`,
+      });
+
+      location.reload();
+    }catch(err){
+      console.log(err);
+    }} else if(type === 'lecture'){
+      try{
+        await axios({
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Token ${document.cookie.split('=')[1]}`
+          },
+          method: 'delete',
+          url: `${DELETE_LECTURE_FILE}${id}`
+        })
+        location.reload();
+      }catch(err){
+        console.log(err);
+      }
+    }
+  }
+
+  const download = () => {
+    fileDownload(file_data, title);
   }
 
   return (
@@ -30,16 +79,28 @@ function Card(props) {
             className='icon'
             className='icon'
             onClick={goLink}
-          /> :
+          /> : type === 'lecture' ?
           <>
-            <BsDownload size='28' title='download' className='icon'/>
+            <BsDownload size='28' title='download' className='icon' onClick={download}/>
             <BsQuestionDiamond size='28' title='quiz' className='icon'/>
-          </>
+          </> :
+          <></>
         }
-        <RiDeleteBin5Line size='28' title='delete' className='icon'/>
+        <RiDeleteBin5Line
+          size='28'
+          title='delete'
+          className='icon'
+          onClick={deleteFile}
+        />
       </div>
       <div className='content-wrapper'>
-        <img width='150px' height='180px' src={img}/>
+        {
+          type === 'url' ? 
+          <img width='180px' height='180px' src={img}/> :
+          type === 'lecture' ?
+          <GrDocumentPdf className='icon'/> :
+          <GrNotes className='icon'/>
+        }
         <div className='content'>
           <div className='title'>{title}</div>
           <div className='summary'>
