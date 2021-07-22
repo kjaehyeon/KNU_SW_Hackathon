f12ae092def6a899ff83fe22f4eb85e5f914ced2
hyunjun <nhj2927@naver.com>
2021-07-22 18:50:26 +0900

    회원가입 기능 추가

--- client/src/Pages/Register/index.js
+++ client/src/Pages/Register/index.js
@@ -1,17 +1,92 @@
+/*eslint-disable*/
 import React from 'react';
-import './style.scss';
+import axios from 'axios';
+import useInput from 'utils/useInput';
+import { REGISTER } from 'Configs/api';
 import {FcCollaboration} from 'react-icons/fc';
+import { useCookies } from "react-cookie";
+import { useHistory } from 'react-router-dom';
+
+import './style.scss';
 const Register = () => {
+  const history = useHistory();
+  const [cookies, setCookie] = useCookies(['token']);
+  const [input, handleInput] = useInput({
+    id: '',
+    email: '',
+    password: '',
+  });
+
+  const {
+    id,
+    email,
+    password
+  } = input;
+
+  const register = async (e) => {
+    e.preventDefault();
+    try{
+    const response = await axios({
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      method: 'post',
+      url: REGISTER,
+      data: {
+        username: id,
+        email,
+        password
+      },
+    });
+
+    const {data: {token}} = response;
+    const {data: {user: {username}}} = response;
+
+    if(token){
+      const expires = new Date();
+      expires.setDate(Date.now() + 1000 * 60 * 60 * 24 * 14)
+
+      setCookie('token', token, {path: '/', expires});
+
+      if(username){
+        localStorage.setItem('username', username);
+      }
+    }
+
+    history.push('/');
+    }catch(err){
+      console.log(err);
+    }
+  }
+
   return (
     <div className="Register">
       <div className="inner">
         <div className="inner1">
           <h1>Query 계정 만들기</h1>
-          <form>
-            <input type ="text" placeholder = "이름을 입력 하세요"></input>
-            <input type="text" placeholder = "아이디를 입력 하세요"></input>
-            <input type="password" placeholder = "비밀번호를 입력 하세요"></input>
-            <button>Sign up</button>
+          <form onSubmit={register}>
+            <input
+              type ="email"
+              name='email'
+              placeholder = "이메일을 입력 하세요"
+              value={email}
+              onChange={handleInput}
+            />
+            <input
+              type="text"
+              name='id'
+              placeholder = "아이디를 입력 하세요"
+              value={id}
+              onChange={handleInput}
+            />
+            <input
+              type="password"
+              name='password'
+              placeholder = "비밀번호를 입력 하세요"
+              value={password} 
+              onChange={handleInput}
+            />
+            <button type='submit'>Sign up</button>
           </form>
         </div>
         <div className="inner2">
